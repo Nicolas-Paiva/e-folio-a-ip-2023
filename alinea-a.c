5ea3f83ee45101e263044fe09957fd4dee63b039
Nícolas Paiva <126703324+Nicolas-Paiva@users.noreply.github.com>
2023-11-28 12:47:24 +0000

    Update alinea-a.c

--- alinea-a.c
+++ alinea-a.c
@@ -1,99 +1,53 @@
-/*
+#include <stdio.h>
+#include <stdlib.h>
 
-OBJETIVO:
+const unsigned int maxCartas = 64;
+const unsigned int valorMaxCarta = 63;
+const unsigned int valorMinCarta = 0;
+const unsigned int tamanhoDaCarta = 4; // Leva em consideração o '\0'
 
-Gerar carta do baralho de acordo com o número introduzido pelo usuário. As cartas devem estar entre 0 e 64
-
-
-Soluções passo a passo:
-
-1. Garantir que o número introduzido pelo usuário esteja dentro dos limites
-
-2. Desenvolver a lógica necessária para gerar a carta correta.
-
-
-*/
-
-// Verifica se o valor introduzido pelo usuário está dentro dos limites
-int verificarValor(int valorIntroduzido, int valorMinimo, int valorMaximo);
-
-// Gera a carta do usuário de acordo com o valor introduzido
+// Gera uma carta de acordo com o valor introduzido
 void gerarCarta(int numeroIntroduzido, char carta[4]);
 
-void gerarCartaOperacao(int numeroIntroduzido, char carta[4], char operacao);
-
-#include <stdio.h>
-
-int main(void)
+int main()
 {
-  char carta[4];
-  int numeroDaCarta;
-  scanf("%d", &numeroDaCarta);
+  char carta[tamanhoDaCarta];
+  int numCarta;
 
-  gerarCarta(numeroDaCarta, carta);
+  scanf("%d", &numCarta);
+  gerarCarta(numCarta, carta);
+
+  printf("%s\n", carta);
 
   return 0;
 }
 
-int verificarValor(int valorIntroduzido, int valorMinimo, int valorMaximo)
-{
-  if (valorIntroduzido < valorMinimo)
-  {
-    return 0;
-  }
-  else if (valorIntroduzido > valorMaximo)
-  {
-    return 0;
-  }
-  return 1;
-}
-
 void gerarCarta(int numeroIntroduzido, char carta[4])
 {
-  // Variaveis defiidas dentro da funcao mostrar carta, pois caso os limites precisem ser alterados, podemos modificar apenas aqui...
-  int cartaMinima = 0;
-  int cartaMaxima = 63;
-  if (verificarValor(numeroIntroduzido, cartaMinima, cartaMaxima) == 0)
+  if (numeroIntroduzido < valorMinCarta || numeroIntroduzido > valorMaxCarta)
   {
     printf("Carta invalida\n");
-    return;
+    exit(1); // Para não mostrar nenhuma carta
   }
 
-  // Se o valor introduzido estiver entre 0 e 15, geramos uma carta com a operacao soma.
-  if (verificarValor(numeroIntroduzido, 0, 15) == 1)
-  {
-    gerarCartaOperacao(numeroIntroduzido, carta, '+');
-  }
-  else if (verificarValor(numeroIntroduzido, 16, 31))
-  {
-    gerarCartaOperacao(numeroIntroduzido, carta, '-');
-  }
-}
+  char letrasDasCartas[4] = {'A', 'B', 'C', 'D'};
+  char operacoesDasCartas[4] = {'+', '-', '*', '/'};
+  char operacaoDaCarta;
+  char numeroDaCarta;
+  char letraDaCarta;
 
-void gerarCartaOperacao(int numeroIntroduzido, char carta[4], char operacao)
-{
-  char operacaoDaCarta = operacao;
-  int numeroDaCarta = '1';
-  char letraDaCarta = 'A';
+  int indexOperacoes = numeroIntroduzido / 16; // Determina a operacao
+  int indexLetra = numeroIntroduzido % 4;      // Determina a letra
 
-  for (int i = 0; i <= numeroIntroduzido; i++)
-  {
-    if (i > 0)
-    {
-      letraDaCarta++;
-    }
-    if (i % 4 == 0 && i != 0)
-    {
-      numeroDaCarta++;
-      letraDaCarta = 'A';
-    }
-    if (i == numeroIntroduzido)
-    {
-      printf("Carta %d: %c%c%c\n", i, operacaoDaCarta, numeroDaCarta, letraDaCarta);
-      carta[0] = operacaoDaCarta;
-      carta[1] = numeroDaCarta;
-      carta[2] = letraDaCarta;
-      carta[3] = '\0';
-    }
-  }
+  operacaoDaCarta = operacoesDasCartas[indexOperacoes];
+
+  letraDaCarta = letrasDasCartas[indexLetra];
+
+  // A cada 4 numeros, o número da carta aumenta, mas a cada 16, volta a '1'
+  numeroDaCarta = '1' + ((numeroIntroduzido % 16) / 4);
+
+  carta[0] = operacaoDaCarta;
+  carta[1] = numeroDaCarta;
+  carta[2] = letraDaCarta;
+  carta[3] = '\0';
 }
